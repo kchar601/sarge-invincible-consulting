ad773174df73aedf50c4cea989155bd46f448b24
Keith Charlton <kcc5354@psu.edu>
2024-04-22 21:29:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const session = require('express-session');
 const path = require('path');
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
-var cookie = require('cookie');
 const nodemailer = require('nodemailer');
 const app = express()
 const port = 80
@@ -14,17 +12,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    secure: false,
-    httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000
-  }
-}));
 
 const transporter = nodemailer.createTransport({
   host: 'mail.smtp2go.com',
@@ -41,9 +28,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-
   console.log(`Example app listening at http://localhost:${port}`)
-
 })
 
 app.get('/Home', (req, res) => {
